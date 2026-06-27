34a754f7a0627e95c47a3c76e7f8a4bfad094913
Aman Sahu <amansahuxy@gmail.com>
2026-06-27 22:32:07 +0530

    Select DOM elements for table generator

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ const input=document.getElementById("input-btn");
 const text=document.getElementById("txt-area");
 const btn=document.getElementById("generate");
 
+btn.addEventListener("click",()=>{
+    let number = Number(input.value);
+       text.value = "";
+    for(let i=number;i<=number*10;i=i+number){
+        text.value+=i+"\n";
+    }
+})
